baf51ed4f76f0db87a9218244a62b7863db2fcbe
elmehdi007 <eguettar@gmail.com>
2019-05-08 23:51:53 +0100

    init

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+/**  
+$('#full-screen-section').contextmenu(function () {
+    return false;
+});
+
+document.getElementById('full-screen-section').onselectstart = new Function("return false");
+document.getElementById('sliders-containner').onselectstart = new Function("return false");
+**/
